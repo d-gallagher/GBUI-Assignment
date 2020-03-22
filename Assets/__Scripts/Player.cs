57c47e380c5fb61c0b8304d251a5324089c70f82
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-22 17:07:59 +0000

    Added scripts Player and PlayerController

---
+++ Assets/__Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+public class Player : MonoBehaviour
+{
+	/// <summary>
+	/// Player Movement Speed
+	/// </summary>
+	public float moveSpeed = 5;
+
+	private Camera _cam;
+	private PlayerController _controller;
+
+	void Start()
+	{
+		_controller = GetComponent<PlayerController>();
+		_cam = Camera.main;
+	}
+
+	void Update()
+	{
+		Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+		_controller.Move(moveVelocity);
+
+		Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+		Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+
+		if (groundPlane.Raycast(ray, out float rayDistance))
+		{
+			Vector3 point = ray.GetPoint(rayDistance);
+			Debug.DrawLine(ray.origin, point, Color.red);
+			//Debug.DrawRay(ray.origin,ray.direction * 100,Color.red);
+			_controller.LookAt(point);
+		}
+	}
+}
---
+++ Assets/__Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// PlayerController - Controls player movement.
+/// <para>Expects Rigidbody component on GameObject</para>
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 _velocity;
+    private Rigidbody _rb;
+
+    private void Start() => _rb = GetComponent<Rigidbody>();
+
+    /// <summary>
+    /// Move the GameObject at a certain velocity.
+    /// </summary>
+    /// <param name="_velocity"></param>
+    public void Move(Vector3 _velocity) => this._velocity = _velocity;
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    /// <summary>
+    /// Physics update.
+    /// </summary>
+    public void FixedUpdate() => _rb.MovePosition(_rb.position + _velocity * Time.fixedDeltaTime);
+}
