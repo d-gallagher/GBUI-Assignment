1b4430d2f5fb605a971ffa9b2dd52f3c5c32f8c1
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-22 18:12:38 +0000

    Added IDamageable and BaseLivingEntity

---
+++ Assets/__Scripts/Core/BaseLivingEntity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Base class for any 'Living' game entities.
+/// <para>Implementation of IDamageable.</para>
+/// </summary>
+public abstract class BaseLivingEntity : MonoBehaviour, IDamageable
+{
+    #region Public Variables
+    public float startingHealth;
+    #endregion
+
+    #region Protected Variables
+    protected float health;
+    protected bool dead;
+    #endregion
+
+    #region Unity Methods
+    protected virtual void Start() => health = startingHealth;
+    #endregion
+
+    public void TakeHit(float damage, RaycastHit hit)
+    {
+        health -= damage;
+
+        if (health <= 0 && !dead) Die();
+    }
+
+    protected void Die()
+    {
+        dead = true;
+        GameObject.Destroy(gameObject);
+    }
+}
---
+++ Assets/__Scripts/Core/IDamageable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Interface defining behaviour for any entity which can take damage.
+/// </summary>
+public interface IDamageable
+{
+	void TakeHit(float damage, RaycastHit hit);
+}
