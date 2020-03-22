d1dd37ea7c2726a32713fd45b908d12cff12cac1
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-22 17:30:07 +0000

    Added Projectile script

---
+++ Assets/__Scripts/Core/Projectile.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Projectile behaviour script.
+/// <para>Script to be used on weapon projectiles or similar</para>
+/// </summary>
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10;
+
+    private void Update() => transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    public void SetSpeed(float newSpeed) => speed = newSpeed;
+}
