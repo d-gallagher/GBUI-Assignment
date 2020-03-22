41a2abfd4c5ef4d51c3a3c1fe2c0066ffa17962c
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-22 17:35:34 +0000

    Added Gun script

---
+++ Assets/__Scripts/Core/Gun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	/// <summary>
+	/// Postion of the end of the gun barrel.
+	/// </summary>
+	public Transform gunBarrelPosition;
+	public Projectile projectile;
+
+	/// <summary>
+	/// Time in milliseconds between each gunshot.
+	/// </summary>
+	public float timeBetweenShots = 100;
+	/// <summary>
+	/// Initial speed of projectile fired from gun.
+	/// </summary>
+	public float shotVelocity = 35;
+
+	private float _nextShotTime;
+
+	public void Shoot()
+	{
+
+		if (Time.time > _nextShotTime)
+		{
+			_nextShotTime = Time.time + timeBetweenShots / 1000;
+			Projectile newProjectile = Instantiate(projectile, gunBarrelPosition.position, gunBarrelPosition.rotation) as Projectile;
+			newProjectile.SetSpeed(shotVelocity);
+		}
+	}
+}
