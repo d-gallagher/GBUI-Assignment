5d84b2923a5dc9e85c9205e4216e28c610eb7ab0
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-22 20:33:38 +0000

    Fixed mixup with Editor script

--- Assets/__Scripts/Core/Editor/MapEditor.cs
+++ Assets/__Scripts/Core/Editor/MapEditor.cs
@@ -1,33 +1,14 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
 
-public class MapEditor : MonoBehaviour
+[CustomEditor(typeof(MapGenerator))]
+public class MapEditor : Editor
 {
-    #region Public Variables
-    public Transform tilePrefab;
-    public Vector2 mapSize;
-    [Range(0, 1)]
-    public float outlinePercent;
-    #endregion
-
-    private void Start() => GenerateMap();
-
-    public void GenerateMap()
+    public override void OnInspectorGUI()
     {
-        string holderName = "Generated Map";
-        if (transform.Find(holderName)) DestroyImmediate(transform.Find(holderName).gameObject);
+        base.OnInspectorGUI();
 
-        Transform mapHolder = new GameObject(holderName).transform;
-        mapHolder.parent = transform;
+        MapGenerator map = target as MapGenerator;
 
-        for (int x = 0; x < mapSize.x; x++)
-        {
-            for (int y = 0; y < mapSize.y; y++)
-            {
-                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0, -mapSize.y / 2 + 0.5f + y);
-                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
-                newTile.localScale = Vector3.one * (1 - outlinePercent);
-                newTile.parent = mapHolder;
-            }
-        }
+        map.GenerateMap();
     }
 }
