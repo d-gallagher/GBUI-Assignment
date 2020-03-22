48c23393f9d7e233184727616a793c912fa87c96
SerjiVutinss <SerjiVutinss@gmail.com>
2020-03-22 19:47:31 +0000

    Added Enemy Attack() and related code

--- Assets/__Scripts/Core/Enemy.cs
+++ Assets/__Scripts/Core/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.AI;
+using static Enums;
 
 /// <summary>
 /// Enemy Script - Basic Enemy AI script.
@@ -10,21 +11,88 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : BaseLivingEntity
 {
-    #region Private Variables
+
+    #region Private/Serialized Variables
+    [SerializeField]
+    private EnemyState _currentState;
+    [SerializeField]
+    float attackDistanceThreshold = .5f;
+    [SerializeField]
+    float timeBetweenAttacks = 1;
+
     private NavMeshAgent _pathfinder;
     private Transform _target;
+    private Material _skinMaterial;
+    private Color _originalColour;
+
+    private float _nextAttackTime;
+    private float _myCollisionRadius;
+    private float _targetCollisionRadius;
     #endregion
 
     #region Unity Methods
     protected override void Start()
     {
         base.Start();
+
+        // Appearance
+        _skinMaterial = GetComponent<Renderer>().material;
+        _originalColour = _skinMaterial.color;
+
+        // Pathfinding
         _pathfinder = GetComponent<NavMeshAgent>();
+        _currentState = EnemyState.Chasing;
         _target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        // Set up other variables.
+        _myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        _targetCollisionRadius = _target.GetComponent<CapsuleCollider>().radius;
+
         // Begin the coroutine to follow the Player.
         StartCoroutine(UpdatePath());
     }
+
+    private void Update()
+    {
+        if (Time.time > _nextAttackTime)
+        {
+            float sqrDstToTarget = (_target.position - transform.position).sqrMagnitude;
+            if (sqrDstToTarget < Mathf.Pow(attackDistanceThreshold + _myCollisionRadius + _targetCollisionRadius, 2))
+            {
+                _nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
     #endregion
+
+    IEnumerator Attack()
+    {
+        _currentState = EnemyState.Attacking;
+        _pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 dirToTarget = (_target.position - transform.position).normalized;
+        Vector3 attackPosition = _target.position - dirToTarget * (_myCollisionRadius);
+
+        float attackSpeed = 3;
+        float percent = 0;
+
+        _skinMaterial.color = Color.red;
+
+        while (percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = (-Mathf.Pow(percent, 2) + percent) * 4;
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        _skinMaterial.color = _originalColour;
+        _currentState = EnemyState.Chasing;
+        _pathfinder.enabled = true;
+    }
 
     IEnumerator UpdatePath()
     {
