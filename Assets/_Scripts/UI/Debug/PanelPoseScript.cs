8e195098ec25a2e33be3adc96340d57daf969b8d
d-gallagher <jibisjib@gmail.com>
2020-03-31 17:09:28 +0100

    Updated Pose instructions on pose panel

--- Assets/_Scripts/UI/Debug/PanelPoseScript.cs
+++ Assets/_Scripts/UI/Debug/PanelPoseScript.cs
@@ -16,7 +16,7 @@ public class PanelPoseScript : MonoBehaviour
     public void Initialise(Thalmic.Myo.Pose pose, Color activeColor, Color inActiveColor)
     {
         this.pose = pose;
-        _text.text = this.pose.ToString();
+        _text.text = this.pose.ToString()+"\n"+GetPoseAction(pose);
         _activeColor = activeColor;
         _inActiveColor = inActiveColor;
     }
@@ -24,4 +24,36 @@ public class PanelPoseScript : MonoBehaviour
     private void Update() => _image.color = _isActive ? _activeColor : _inActiveColor;
 
     public void SetActive(bool isActive) => _isActive = isActive;
+
+    private string GetPoseAction(Thalmic.Myo.Pose pose)
+    {
+        string poseAction = "pose";
+        switch (pose)
+        {
+            case Thalmic.Myo.Pose.Rest:
+                poseAction = "";
+                break;
+            case Thalmic.Myo.Pose.Fist:
+                poseAction = "Fire Primary Weapon";
+                break;
+            case Thalmic.Myo.Pose.WaveIn:
+                poseAction = "Previous Weapon";
+                break;
+            case Thalmic.Myo.Pose.WaveOut:
+                poseAction = "Next Weapon";
+                break;
+            case Thalmic.Myo.Pose.FingersSpread:
+                poseAction = "Secondary Weapon";
+                break;
+            case Thalmic.Myo.Pose.DoubleTap:
+                poseAction = "Reset Aim - 12 O'Clock";
+                break;
+            case Thalmic.Myo.Pose.Unknown:
+                poseAction = "";
+                break;
+            default:
+                break;
+        }
+        return poseAction;
+    }
 }
