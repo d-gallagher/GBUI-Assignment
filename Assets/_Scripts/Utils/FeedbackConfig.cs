f39eb5506167a3ae707b5482e07b82c235a7a275
SerjiVutinss <SerjiVutinss@gmail.com>
2020-04-03 01:06:54 +0100

    Updated FeedbackConfig

---
+++ Assets/_Scripts/Utils/FeedbackConfig.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using static Enums;
+
+public class FeedbackConfig : MonoBehaviour
+{
+    public FeedbackType feedbackType;
+
+    public float vibrateDuration;
+
+    public float shakeDuration;
+    public float shakeIntensity;
+    public float shakeDecreaseFactor;
+}
